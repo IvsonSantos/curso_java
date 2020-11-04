8221927099fabceb36f6f1e370ce4f784ce9c617
ivsonsantos <Belgique@2024>
2020-11-04 21:45:29 +0100

    Incremento

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+H
+src/TestaOperadores.java,8/2/8266118492454d19995707fcdc07cb1b821b44cf
--- src/TestaOperadores.java
+++ src/TestaOperadores.java
@@ -1,9 +1,7 @@
 public class TestaOperadores {
 
     public static void main(String[] args) {
-
-        int a = exercicio2();
-        System.out.println(a);
+        incremento();
     }
 
     public static void operadores() {
@@ -42,4 +40,68 @@ public class TestaOperadores {
     public static int exercicio2() {
         return 5 + 4;
     }
+
+    public static void multiplicacao() {
+
+        int d = 3 * 4;
+        System.out.println("D: " + d);
+
+        int a = 5;
+        int b = 4;
+        int c = a * b;
+        System.out.println("C: " + c);
+
+        System.out.println(c * a);
+        System.out.println(5 * 3 * 2);
+        System.out.println(5 * 3 * 2 * a);
+    }
+
+    public static void divisao() {
+
+        /*
+        int d = 15 / 5;
+        System.out.println("D: " + d);
+
+        int a = 30;
+        int b = 3;
+        int c = a / b;
+        System.out.println("C: " + c);
+
+        System.out.println(a / c);
+        System.out.println(10 / 2);
+        System.out.println(12 / b);
+
+         */
+
+        int a = 7 / 2;
+        System.out.println(a);
+
+
+    }
+
+    public static void divisaoPorZero(int a, int b) {
+
+        System.out.println("Início da divisão");
+
+        int c = a - b;
+        int d = 8 / c;
+
+        System.out.println("D: " + d);
+
+    }
+
+    public static void precedenciaDeOperadores() {
+        System.out.println(8 + (2 - 4) * 2);
+    }
+
+    public static void incremento() {
+        int a = 5;
+        a++;   // igual a (a = a + 1)
+        System.out.println("A: " + a);
+
+        int b = 7;
+        System.out.println(b++);  // 7
+        System.out.println(b);    // 8
+    }
+
 }
