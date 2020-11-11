b5cb02805a2ccf5779c731359cc4ac185e2473a3
ivsonsantos <Belgique@2024>
2020-11-11 19:32:28 +0100

    Float

---
+++ src/primitivos/tipofloat/TrabalhandoComFloat.java
@@ -0,0 +1,21 @@
+package primitivos.tipofloat;
+
+public class TrabalhandoComFloat {
+
+    public static void main(String[] args) {
+
+        // operacoes com o float
+        int a = 10/3;
+        System.out.println("A: " + a);
+
+        float b = 10/3;
+        System.out.println("B: " + b);
+
+        float c = 10f/3f;
+        System.out.println("C: " + c);
+
+        float teste = 20f/7f;
+        float resultado = a / 2f;
+        System.out.println("Resultado: " + resultado);
+    }
+}
