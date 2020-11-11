3114d3546f7c8c8e777df8f3444dc07a3cd62be3
ivsonsantos <Belgique@2024>
2020-11-11 20:04:04 +0100

    Double

---
+++ src/primitivos/tipodouble/TestaDouble.java
@@ -0,0 +1,23 @@
+package primitivos.tipodouble;
+
+/**
+ * São números de ponto flutuante de dupla precisão.
+ *
+ * Tamanho de 64 bits.  (float 32 bits)
+ *
+ * É recomendado para ser utilizado com valores decimais, mas não para valores mais precisos, como dinheiro por exemplo.
+ *
+ * Para dinheiro podemos utilizar o java.math.BigDecimal.
+ */
+public class TestaDouble {
+
+    public static void main(String[] args) {
+
+        double d = 10;
+
+        System.out.println("D: " + d);
+
+    }
+
+
+}
---
+++ src/primitivos/tipofloat/ValorPadraoFloat.java
@@ -0,0 +1,20 @@
+package primitivos.tipofloat;
+
+public class ValorPadraoFloat {
+
+    float a;
+
+    public static void main(String[] args) {
+
+        float b;
+
+        ValorPadraoFloat valorPadraoFloat = new ValorPadraoFloat();
+        System.out.println("O valor de A: " + valorPadraoFloat.a);
+
+        b = 5f;
+        System.out.println("B: " + b);
+
+
+    }
+
+}
