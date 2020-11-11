43d10a1b5e46f94bbb19273216e0a0621e7efb89
ivsonsantos <Belgique@2024>
2020-11-11 19:49:18 +0100

    Float

--- src/primitivos/tipofloat/TrabalhandoComFloat.java
+++ src/primitivos/tipofloat/TrabalhandoComFloat.java
@@ -5,6 +5,7 @@ public class TrabalhandoComFloat {
     public static void main(String[] args) {
 
         // operacoes com o float
+        /*
         int a = 10/3;
         System.out.println("A: " + a);
 
@@ -17,5 +18,28 @@ public class TrabalhandoComFloat {
         float teste = 20f/7f;
         float resultado = a / 2f;
         System.out.println("Resultado: " + resultado);
+
+         */
+
+        int a = 10;
+        float b = 10f;
+
+        System.out.println("Divisao com int: " + 0 / a);
+
+        System.out.println("Divisao com float: " + 0 / b);
+
+        // vai dar erro de execucao
+        //System.out.println("Divisao int: " + a / 0);
+
+        System.out.println("Divisao float: " + b / 0);
+
+        // quebra a logica pois retorna infinito
+        float c = b / 0;
+        System.out.println("C: " + c);
+
+        float ultimo = 10f + c;
+        System.out.println("Ultimo: " + ultimo);
+
+
     }
 }
