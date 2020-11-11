3f6b2018ea832907c7e9b6f58f123a9c145fd0fc
ivsonsantos <Belgique@2024>
2020-11-11 20:31:42 +0100

    Double

--- src/primitivos/tipodouble/TestaDouble.java
+++ src/primitivos/tipodouble/TestaDouble.java
@@ -11,11 +11,53 @@ package primitivos.tipodouble;
  */
 public class TestaDouble {
 
+    double e;
+
     public static void main(String[] args) {
 
-        double d = 10;
+        // operacoes com o double
 
-        System.out.println("D: " + d);
+        /*
+        float a = 10f / 3f;
+        System.out.println("A: " + a);
+
+        double c = 10d / 3d;
+        System.out.println("C: " + c);
+
+        // float cabe em um double
+        double teste = 20f / 7f;
+        // erro pois double nao cabe em um float
+        //float teste2 = 20d / 7d;
+
+        double resultado = a / 2d;
+        System.out.println("Resultado: " + resultado);
+         */
+
+
+        // divisao com double
+        int a = 10;
+        double b = 10d;
+
+        System.out.println("Divisao com int: " + 0 / a);
+        System.out.println("Divisao com double: " + 0 / b);
+
+
+        // vai dar erro de execucao
+        //System.out.println("Divisao int: " + a / 0);
+
+        System.out.println("Divisao double: " + b / 0);
+
+        TestaDouble testaDouble = new TestaDouble();
+        System.out.println("Valor padrão: " + testaDouble.e);
+
+        /*
+        // quebra a logica pois retorna infinito
+        float c = b / 0;
+        System.out.println("C: " + c);
+
+        float ultimo = 10f + c;
+        System.out.println("Ultimo: " + ultimo);
+        */
 
     }
 
