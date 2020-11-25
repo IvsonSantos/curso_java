10db56b0cfe16bc7b9b7817ec0a167a0c5a25bf5
ivsonsantos <Belgique@2024>
2020-11-25 20:28:38 +0100

    String Length

---
+++ src/string/StringLength.java
@@ -0,0 +1,17 @@
+package string;
+
+public class StringLength {
+
+    public static void main(String[] args) {
+
+        String a = new String("teste");
+        System.out.println("O tamanho da string a é: " + a.length());
+
+
+        String b = new String("psicologia");
+        System.out.println("O tamanho da string b é: " + b.length());
+
+        System.out.println("Primeiro".length());
+    }
+
+}
